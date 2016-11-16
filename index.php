95ee64e19f96c4e1e19361155e5158f73d8ddfed
espaipruebas <espaipruebas@gmail.com>
2016-11-16 12:27:16 +0100

    Primer commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Dise&ntilde;o Web Responsive con HTML5 y CSS3</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="" />
+    <meta name="keywords" content="css3, html5 responsive diseno, dise&ntilde;o web" />
+    <meta name="author" content="CreativaSfera" />
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <script type="text/javascript" src="js/modernizr.custom.79639.js"></script>
+		<!--[if lte IE 8]>
+			 <link rel="stylesheet" type="text/css" href="css/ie8.css" />
+		<![endif]-->
+</head>
+<body>
+
+    <div class="container">
+
+        <input type="radio" name="radio-set" checked="checked" id="radio-1"/>
+                <a href="#home">Home</a>
+        <input type="radio" name="radio-set" id="radio-2"/>
+                <a href="#sobre">Sobre m&iacute;</a>
+        <input type="radio" name="radio-set"  id="radio-3"/>
+                <a href="#diseno">Dise&ntilde;o</a>
+        <input type="radio" name="radio-set"  id="radio-4"/>
+                <a href="#cursos">Cursos</a>
+        <input type="radio" name="radio-set"  id="radio-5"/>
+                <a href="#contacto">Contacto</a>
+
+
+
+        <div class="scroll">
+
+            <section class="panel color-1" id="home">
+                <h2>Responsive Design</h2>
+                <p>Aprendiendo a crear sitios web responsive con HTML5 y CSS3</p>
+
+             </section>
+
+            <section class="panel color-2" id="sobre">
+                <h2>Lorem ipsum</h2>
+                <p>Lorem ipsum, bla, bla, bla, m&aacute;s lorem ipsum.</p>
+            </section>
+
+            <section class="panel color-3" id="diseno">
+                <h2>Lorem ipsum</h2>
+                <p>Lorem ipsum, bla, bla, bla, m&aacute;s lorem ipsum.</p>
+            </section>
+
+            <section class="panel color-4" id="cursos">
+                <h2>Lorem ipsum</h2>
+                <p>Lorem ipsum, bla, bla, bla, m&aacute;s lorem ipsum.</p>
+            </section>
+
+
+            <section class="panel color-5" id="contacto">
+                 <h2>Lorem ipsum</h2>
+                <p>Lorem ipsum, bla, bla, bla, m&aacute;s lorem ipsum.</p>
+            </section>
+
+
+        </div> <!-- fin del scroll -->
+
+
+    </div> <!-- fin del container -->
+    <div class="clr"></div>
+    <footer>
+     <a href="#">Espa&ntilde;ol</a> &nbsp; | &nbsp; <a href="#">English</a>
+    </footer>
+
+</body>
+</html>
